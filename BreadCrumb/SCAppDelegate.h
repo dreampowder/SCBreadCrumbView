f3a0a608933c83c9085d02bb30774b5fde3340f6
dreampowder <coskun.serdar@gmail.com>
2013-12-06 14:52:02 +0200

    Initial Commit

---
+++ BreadCrumb/SCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCAppDelegate.h
+//  BreadCrumb
+//
+//  Created by Serdar coskun on 06/12/13.
+//  Copyright (c) 2013 Serdar coskun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BreadCrumb/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BreadCrumb
+//
+//  Created by Serdar coskun on 06/12/13.
+//  Copyright (c) 2013 Serdar coskun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SCAppDelegate class]));
+    }
+}
